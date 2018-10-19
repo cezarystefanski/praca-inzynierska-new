2c322df6f74cf62cec862045050c3db58df08565
Cezary Stefanski <cezarystefanski@gmail.com>
2018-10-19 03:10:53 +0200

    add storybook

---
+++ src/client/components/common/ExampleBlock/ExampleBlock.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { ExampleBlock } from './ExampleBlock';
+
+describe('component common/ExampleBlock', () => {
+  const wrapper = shallow(
+    <ExampleBlock text="example text" />,
+  );
+
+  it('should render correctly with a prop', () => {
+    expect(wrapper.find('p'))
+      .toHaveLength(2);
+  });
+
+  it('should not differ from earlier version', () => {
+    expect(wrapper)
+      .toMatchSnapshot();
+  });
+
+  it('should render correctly with no props', () => {
+    const wrapperWithNoProps = shallow(
+      <ExampleBlock />,
+    );
+
+    expect(wrapperWithNoProps.find('p'))
+      .toHaveLength(1);
+  });
+});
