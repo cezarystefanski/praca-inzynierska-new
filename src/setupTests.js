68a0b1558676bffeff31b820a003a2a1130f6642
cezarystefanski <cezarystefanski@gmail.com>
2018-10-19 17:39:16 +0200

    stash

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,10 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+import { StyleSheetTestUtils } from 'aphrodite';
+import { StyleSheetTestUtils as StyleSheetTestUtilsNoImportant } from 'aphrodite/no-important';
+
+StyleSheetTestUtils.suppressStyleInjection();
+StyleSheetTestUtilsNoImportant.suppressStyleInjection();
+
 configure({ adapter: new Adapter() });
