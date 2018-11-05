e0f192bcd7fc7f8c2b1756ccdc8961f8ca1c85b3
cezarystefanski <cezarystefanski@gmail.com>
2018-11-05 16:08:32 +0100

    align with master

--- src/client/components/common/ExampleBlock/ExampleBlock.test.js
+++ src/client/components/common/ExampleBlock/ExampleBlock.test.js
@@ -4,13 +4,9 @@ import { shallow } from 'enzyme';
 import ExampleBlock from './index';
 
 describe('component common/ExampleBlock', () => {
-<<<<<<< HEAD
-  const wrapper = shallow(<ExampleBlock text="example text" />);
-=======
   const wrapper = shallow(
     <ExampleBlock text="example text" />,
   ).shallow();
->>>>>>> 68a0b15... stash
 
   it('should render correctly with a prop', () => {
     expect(wrapper.find('p'))
@@ -23,13 +19,9 @@ describe('component common/ExampleBlock', () => {
   });
 
   it('should render correctly with no props', () => {
-<<<<<<< HEAD
-    const wrapperWithNoProps = shallow(<ExampleBlock />);
-=======
     const wrapperWithNoProps = shallow(
       <ExampleBlock />,
     ).shallow();
->>>>>>> 68a0b15... stash
 
     expect(wrapperWithNoProps.find('p'))
       .toHaveLength(1);
