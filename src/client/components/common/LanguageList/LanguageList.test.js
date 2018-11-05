be1e8b2121ac15c7d6c78e09cb48bffe439ae94b
cezarystefanski <cezarystefanski@gmail.com>
2018-11-05 15:42:52 +0100

    Add tests to LanguageList

--- src/client/components/common/LanguageList/LanguageList.test.js
+++ src/client/components/common/LanguageList/LanguageList.test.js
@@ -11,17 +11,37 @@ describe('component common/LanguageList', () => {
     </IntlProvider>,
   );
 
+  const shallowWrapper = shallow(
+    <LanguageList />,
+  ).shallow();
+
   it('should render correctly', () => {
     expect(wrapper.find('button'))
       .toHaveLength(2);
   });
 
   it('should not differ from earlier version', () => {
-    const shallowWrapper = shallow(
-      <LanguageList />,
-    ).shallow();
-
     expect(shallowWrapper)
       .toMatchSnapshot();
+  });
+
+  it('should change language', () => {
+    const button = wrapper.at(0).find('button').at(0);
+
+    window.location.reload = jest.fn();
+
+    button.simulate('click');
+
+    expect(window.location.reload)
+      .toHaveBeenCalled();
+
+    window.location.reload.mockRestore();
+  });
+
+  it('should not change language when it\'s the same', () => {
+    const wrapperInstance = shallowWrapper.instance();
+
+    expect(wrapperInstance.changeLanguage('pl', 'pl'))
+      .toBeFalsy();
   });
 });
